a0e298a359300b132575671edd32cb39786f77fd
Chujo58 <chloelegue@gmail.com>
2025-12-04 18:37:34 -0500

    Lid Close stuff now works and lock + dpms off/on

---
+++ hypr/scripts/lid-dpms.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+HYPRCTL=/usr/bin/hyprctl
+LOGINCTL=/usr/bin/loginctl
+
+lid_state=$(cat /proc/acpi/button/lid/LID0/state | awk '{print $2}')
+echo $lid_state
+
+if [[ "$lid_state" == "closed" ]]; then
+    $LOGINCTL lock-session
+    $HYPRCTL dispatch dpms off
+else
+    $HYPRCTL dispatch dpms on
+fi
