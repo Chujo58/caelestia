30b590b3a675f3206e8a6bbdbbaa2735de4a98ab
Chloé Legué <chloelegue@gmail.com>
2026-04-30 10:14:27 -0400

    fish: fixing config

--- caelestia/user-config.fish
+++ caelestia/user-config.fish
@@ -1,5 +1,5 @@
-set -l bashrc $HOME/.bashrc
-test -f "$bashrc"; and source "$bashrc"
+# set -l bashrc $HOME/.bashrc
+# test -f "$bashrc"; and source "$bashrc"
 
 zoxide init fish --cmd cd | source
 
@@ -15,3 +15,45 @@ else
     end
 end
 set -e __conda_setup
+
+# if not pgrep --full "ssh-agent" >//dev/null
+# 	eval (ssh-agent -c)
+# 	ssh-add ~/.ssh/id_ed25519
+# end
+
+# Created by `pipx` on 2025-10-28 16:29:26
+set PATH $PATH /home/chujo/.local/bin
+
+# Add bin to path
+fish_add_path ~/bin
+
+# Bashrc stuff
+
+# Aliases
+alias grep='grep --color=auto'
+alias copy='wl-copy'
+
+alias neofetch="fastfetch -c none"
+alias startwal="systemctl --user start caelestia-wallpaper.timer"
+alias stopwal="systemctl --user stop caelestia-wallpaper.timer"
+alias config="cd ~/.local/share/caelestia; code .; exit"
+
+# Mounting aliases:
+## CANFAR:
+alias mount_arc='sshfs clegue@ws-uv.canfar.net:/ $HOME/mounts/canfar -p 64022 -o reconnect -o cache=yes -o kernel_cache -o auto_cache -o attr_timeout=120 -o entry_timeout=120 -o negative_timeout=60 -o compression=no'
+# alias mount_arc='sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=10 -p 64022 clegue@ws-uv.canfar.net:/ $HOME/mounts/canfar'
+alias unmount_arc='umount $HOME/mounts/canfar'
+
+## Windows partition:
+alias mount_win='sudo mount -t ntfs-3g /dev/nvme0n1p3 $HOME/mounts/windows/'
+alias unmount_win="sudo umount $HOME/mounts/windows"
+
+# Fish abbreviations
+abbr py 'python'
+abbr vi 'nvim'
+abbr vim 'nvim'
+
+# Exports
+export LD_LIBRARY_PATH="/usr/lib"
+# export LD_LIBRARY_PATH=/opt/conda/lib:$LD_LIBRARY_PATH
+export EDITOR="nvim"
--- fish/config.fish
+++ fish/config.fish
@@ -44,14 +44,3 @@
     
     source ~/.config/caelestia/user-config.fish 2> /dev/null
 end
-
-# if not pgrep --full "ssh-agent" >//dev/null
-# 	eval (ssh-agent -c)
-# 	ssh-add ~/.ssh/id_ed25519
-# end
-
-# Created by `pipx` on 2025-10-28 16:29:26
-set PATH $PATH /home/chujo/.local/bin
-
-# Add bin to path
-fish_add_path ~/bin
