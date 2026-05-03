8a23ca57a158efbba31f99ba25c561a8e36c3442
Chloé Legué <chloelegue@gmail.com>
2026-05-03 10:34:53 -0400

    scripts/tools: new posthook for theme so im removing my old systemd setup

--- caelestia/posthooks/wallpaper.sh
+++ caelestia/posthooks/wallpaper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo $WALLPAPER_PATH
 
 # AWWW for animated wallpapers
 awww img ~/.local/state/caelestia/wallpaper/current --transition-type grow --transition-pos center --transition-duration 2
