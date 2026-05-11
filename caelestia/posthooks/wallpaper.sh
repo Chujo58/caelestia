0ec372256162ca7223fb3776c702fb264a1d3c60
Chloé Legué <chloelegue@gmail.com>
2026-05-11 11:49:02 -0400

    config changes + obsidian template

--- caelestia/posthooks/wallpaper.sh
+++ caelestia/posthooks/wallpaper.sh
@@ -3,3 +3,4 @@ echo $WALLPAPER_PATH
 
 # AWWW for animated wallpapers
 awww img ~/.local/state/caelestia/wallpaper/current --transition-type grow --transition-pos center --transition-duration 2
+# cp "$WALLPAPER_PATH" /usr/share/sddm/themes/pixie/assets/background.jpg
